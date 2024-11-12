b5d9e84f808dc758712873cdb06302bed1f33ef6
Funtimes909 <funtimes90969@gmail.com>
2024-11-12 14:34:10 +1300

    /stats command

--- src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Stats.java
+++ src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Stats.java
@@ -1,9 +1,36 @@
 package xyz.funtimes909.serverseekerv2_discord_bot.commands;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import xyz.funtimes909.serverseekerv2_discord_bot.Main;
+import xyz.funtimes909.serverseekerv2_discord_bot.util.DatabaseConnectionPool;
+
+import java.awt.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Stats {
     public static void stats(SlashCommandInteractionEvent event) {
+        try (Connection connection = DatabaseConnectionPool.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM servers");
+            resultSet.next();
+            int serverCount = resultSet.getInt("count");
 
+            MessageEmbed embed = new EmbedBuilder()
+                    .setTitle("Stats")
+                    .setAuthor("ServerSeekerV2", "https://discord.gg/WEErxAP8kz", "https://funtimes909.xyz/assets/images/serverseekerv2-icon-cropped.png")
+                    .setFooter("Funtimes909", "https://funtimes909.xyz/avatar-gif")
+                    .addField("Unique Servers Found", "**" + serverCount + "**", false)
+                    .setColor(new Color(92, 0, 255))
+                    .build();
+
+            event.replyEmbeds(embed).queue();
+        } catch (SQLException e) {
+            Main.logger.error("Error running the stats command!", e);
+        }
     }
 }
