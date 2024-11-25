4235091510f33b6e3e72866a4ce1d7f1cd4a0909
Funtimes909 <funtimes90969@gmail.com>
2024-11-25 20:02:07 +1300

    organize CommandRegisterer better

--- src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/util/CommandRegisterer.java
+++ src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/util/CommandRegisterer.java
@@ -5,46 +5,38 @@ import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CommandRegisterer {
     public static void registerCommands(JDA client) {
-
-        List<OptionData> searchOptions = new ArrayList<>();
-        searchOptions.add(new OptionData(OptionType.STRING, "player", "Search for servers that a player is currently playing on"));
-        searchOptions.add(new OptionData(OptionType.STRING, "version", "Search for servers using a specific minecraft version"));
-        searchOptions.add(new OptionData(OptionType.STRING, "description", "Search for servers with a specific description"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "playercount", "Search for servers with a specific amount of players online"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "maxplayers", "Search for servers that have a specific player cap"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "preventsreports", "Search for servers that prevent chat reports"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "enforcesecure", "Search for servers that enforce secure chat"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "cracked", "Search for servers that are running in offline mode"));
-        searchOptions.add(new OptionData(OptionType.STRING, "country", "Search for servers running in a specific country"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "port", "Search for servers running on a specific port"));
-        searchOptions.add(new OptionData(OptionType.STRING, "asn", "Search for servers running from a specific ASN"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "seenbefore", "Search for servers scanned before a specific unix timestamp"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "seenafter", "Search for servers scanned after a specific unix timestamp"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "timesseen", "Search for servers that have been scanned a specific time"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "icon", "Search for servers that have an icon"));
-        searchOptions.add(new OptionData(OptionType.STRING, "mods", "Search for forge servers running a specific ModID"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "full", "Search for servers with a full player count"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "empty", "Search for servers that have no players online"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "protocol", "Search for servers using a specific protocol"));
-        searchOptions.add(new OptionData(OptionType.BOOLEAN, "forge", "Search for servers running Forge"));
-        searchOptions.add(new OptionData(OptionType.INTEGER, "forgeversion", "Search for servers running a specific FML Network version"));
-        searchOptions.add(new OptionData(OptionType.STRING, "hostname", "Reverse DNS name of the server"));
-
         client.updateCommands().addCommands(
                 Commands.slash("search", "Searches for servers with advanced filters")
-                                    .addOptions(searchOptions),
+                        .addOption(OptionType.STRING, "player", "Search for servers that a player is currently playing on")
+                        .addOption(OptionType.STRING, "version", "Search for servers using a specific minecraft version")
+                        .addOption(OptionType.STRING, "description", "Search for servers with a specific description")
+                        .addOption(OptionType.INTEGER, "playercount", "Search for servers with a specific amount of players online")
+                        .addOption(OptionType.INTEGER, "maxplayers", "Search for servers that have a specific player cap")
+                        .addOption(OptionType.BOOLEAN, "preventsreports", "Search for servers that prevent chat reports")
+                        .addOption(OptionType.BOOLEAN, "enforcesecure", "Search for servers that enforce secure chat")
+                        .addOption(OptionType.BOOLEAN, "cracked", "Search for servers that are running in offline mode")
+                        .addOption(OptionType.STRING, "country", "Search for servers running in a specific country")
+                        .addOption(OptionType.INTEGER, "port", "Search for servers running on a specific port")
+                        .addOption(OptionType.STRING, "asn", "Search for servers running from a specific ASN")
+                        .addOption(OptionType.INTEGER, "seenbefore", "Search for servers scanned before a specific unix timestamp")
+                        .addOption(OptionType.INTEGER, "seenafter", "Search for servers scanned after a specific unix timestamp")
+                        .addOption(OptionType.INTEGER, "timesseen", "Search for servers that have been scanned a specific time")
+                        .addOption(OptionType.BOOLEAN, "icon", "Search for servers that have an icon")
+                        .addOption(OptionType.STRING, "mods", "Search for forge servers running a specific ModID")
+                        .addOption(OptionType.BOOLEAN, "full", "Search for servers with a full player count")
+                        .addOption(OptionType.BOOLEAN, "empty", "Search for servers that have no players online")
+                        .addOption(OptionType.INTEGER, "protocol", "Search for servers using a specific protocol")
+                        .addOption(OptionType.BOOLEAN, "forge", "Search for servers running Forge")
+                        .addOption(OptionType.INTEGER, "forgeversion", "Search for servers running a specific FML Network version")
+                        .addOption(OptionType.STRING, "hostname", "Reverse DNS name of the server"),
 
-                Commands.slash("random", "Finds a random server"),
                 Commands.slash("blacklist", "Blacklist a user from using the bot")
+                        .addOption(OptionType.USER, "user", "Which user add/remove from the blacklist")
                         .addOptions(new OptionData(OptionType.STRING, "operation", "Whether to add or remove a user from the blacklist")
                                 .addChoice("add", "Add")
-                                .addChoice("remove", "Remove"))
-                        .addOption(OptionType.USER, "user", "Which user add/remove from the blacklist"),
+                                .addChoice("remove", "Remove")),
 
                 Commands.slash("playerinfo", "Shows information about a player")
                                 .addOption(OptionType.STRING, "player", "The player to show", true),
@@ -57,10 +49,11 @@ public class CommandRegisterer {
                         .addOption(OptionType.STRING, "player", "The player you want to search for", false)
                         .addOption(OptionType.STRING, "address", "The server you want to search history for", false),
 
-                Commands.slash("takedown", "Request for your server to be excluded from all future scans")
+                Commands.slash("takedown", "Adds an ip address to the exclude for the next scan, optionally removes all entries of that address from the database")
                         .addOption(OptionType.STRING, "address", "IP address of the server you want to takedown", true)
                         .addOption(OptionType.BOOLEAN, "remove-entries", "Whether or not to delete existing entries of your server from the database"),
 
+                Commands.slash("random", "Finds a random server"),
                 Commands.slash("info", "Displays information about the bot, source code, author and credits"),
                 Commands.slash("stats", "Shows stats about the bot"),
                 Commands.slash("ping", "Ping a specific server.")
