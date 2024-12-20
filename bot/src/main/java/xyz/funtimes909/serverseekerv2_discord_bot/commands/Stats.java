ed171a83a5dfb7f05eeb48159bdb2594bbd40b3c
Funtimes909 <funtimes90969@gmail.com>
2024-12-20 18:31:27 +1300

    port /stats to use the API

--- bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Stats.java
+++ bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Stats.java
@@ -1,9 +1,12 @@
 package xyz.funtimes909.serverseekerv2_discord_bot.commands;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import xyz.funtimes909.serverseekerv2_discord_bot.Main;
+import xyz.funtimes909.serverseekerv2_discord_bot.util.APIUtils;
 import xyz.funtimes909.serverseekerv2_discord_bot.util.ConnectionPool;
 
 import java.awt.*;
@@ -14,25 +17,24 @@ import java.sql.Statement;
 
 public class Stats {
     public static void stats(SlashCommandInteractionEvent event) {
-        try (Connection connection = ConnectionPool.getConnection()) {
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery("SELECT COUNT(*) FROM servers");
-            results.next();
-            int serverCount = results.getInt("count");
+        JsonElement response = APIUtils.api("stats");
 
-            MessageEmbed embed = new EmbedBuilder()
-                    .setTitle("Stats")
-                    .setColor(new Color(0, 255, 0))
-                    .setAuthor("ServerSeekerV2", "https://discord.gg/WEErxAP8kz", "https://funtimes909.xyz/assets/images/serverseekerv2-icon-cropped.png")
-                    .setFooter("Funtimes909", "https://funtimes909.xyz/avatar-gif")
-                    .addField("Unique Servers Found", "**" + serverCount + "**", false)
-                    .build();
-
-            event.getHook().sendMessageEmbeds(embed).queue();
-            results.close();
-            statement.close();
-        } catch (SQLException e) {
-            Main.logger.error("Error running the stats command!", e);
+        if (response == null || !response.isJsonObject()) {
+            event.getHook().sendMessage("No results!").queue();
+            return;
         }
+
+        JsonObject object = response.getAsJsonObject();
+        int serverCount = object.get("servers").getAsInt();
+
+        MessageEmbed embed = new EmbedBuilder()
+                .setTitle("Stats")
+                .setColor(new Color(0, 255, 0))
+                .setAuthor("ServerSeekerV2", "https://discord.gg/WEErxAP8kz", "https://funtimes909.xyz/assets/images/serverseekerv2-icon-cropped.png")
+                .setFooter("Funtimes909", "https://funtimes909.xyz/avatar-gif")
+                .addField("Unique Servers Found", "**" + serverCount + "**", false)
+                .build();
+
+        event.getHook().sendMessageEmbeds(embed).queue();
     }
 }
