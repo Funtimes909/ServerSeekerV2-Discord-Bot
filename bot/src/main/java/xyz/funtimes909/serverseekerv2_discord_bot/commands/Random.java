594f6d3885f01fb1b16567ad40d4175098252a7f
Funtimes909 <funtimes90969@gmail.com>
2024-12-21 16:47:04 +1300

    Fix compiling

--- bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Random.java
+++ bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/commands/Random.java
@@ -8,6 +8,10 @@ import xyz.funtimes909.serverseekerv2_core.records.Server;
 import xyz.funtimes909.serverseekerv2_core.util.ServerObjectBuilder;
 import xyz.funtimes909.serverseekerv2_discord_bot.builders.ServerEmbedBuilder;
 import xyz.funtimes909.serverseekerv2_discord_bot.util.APIUtils;
+import xyz.funtimes909.serverseekerv2_discord_bot.util.GenericErrorEmbed;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Random {
     public static void random(SlashCommandInteractionEvent event) {
@@ -21,13 +25,17 @@ public class Random {
         JsonObject object = response.getAsJsonObject();
         Server server = ServerObjectBuilder.buildServerFromApiResponse(object);
         ServerEmbedBuilder embedBuilder = new ServerEmbedBuilder(server);
-        MessageEmbed embed = embedBuilder.build(false);
+        CompletableFuture<MessageEmbed> embed = embedBuilder.build(event.getChannel(), false);
 
         if (embed == null) {
             event.getHook().sendMessage("No results!").queue();
             return;
         }
 
-        event.getHook().sendMessageEmbeds(embed).queue();
+        try {
+            event.getHook().sendMessageEmbeds(embed.get()).queue();
+        } catch (ExecutionException | InterruptedException e) {
+            GenericErrorEmbed.errorEmbed(event.getChannel(), e.getMessage());
+        }
     }
 }
