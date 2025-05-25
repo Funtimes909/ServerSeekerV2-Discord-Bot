cb6dd8d0c23825dd0d4c7389a28a14e168b5ccbb
funtimes909 <funtimes90969@proton.me>
2025-05-26 00:12:01 +1200

    fix(SearchEmbedBuilder): Ensure fields are always the same length (long-standing issue!!)

--- bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/SearchEmbedBuilder.java
+++ bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/SearchEmbedBuilder.java
@@ -16,43 +16,46 @@ public class SearchEmbedBuilder {
                 .setAuthor("ServerSeekerV2", "https://discord.gg/UA5kyprunc", "https://cdn.discordapp.com/app-icons/1375333922765930556/bcc3069c7e9fdb44107faeb74477127d.png?size=256")
                 .setFooter("Funtimes909", "https://funtimes909.xyz/assets/images/floppa.png");
 
-        int longestAddress = 0;
-        for (JsonElement entry : servers) {
-            JsonObject object = entry.getAsJsonObject();
-
-            if (object.get("address").getAsString().length() > longestAddress) {
-                longestAddress = object.get("address").getAsString().length();
-            }
-        }
-
         int index = 1;
         for (JsonElement server : servers) {
             JsonObject entry = server.getAsJsonObject();
 
-            StringBuilder address = new StringBuilder("``").append(entry.get("address").getAsString()).append(":").append(entry.get("port").getAsString()).append("``");
-            StringBuilder version = new StringBuilder("``").append(entry.get("version").getAsString()).append("``");
-            String timestamp = "<t:" + entry.get("last_seen") + ":R>";
+            StringBuilder address = new StringBuilder(String.format("``%s:%d``", entry.get("address").getAsString(), entry.get("port").getAsShort()));
+            StringBuilder version = new StringBuilder(String.format("``%s``", entry.get("version").getAsString()));
 
+            // Add the countries flag before the index if it exists
             if (!entry.get("country").isJsonNull()) {
                 address.insert(0, ":flag_" + entry.get("country").getAsString().toLowerCase() + ": : ");
+
+                // Ensure it's all the same length
+                while (address.length() < 38) {
+                    address.insert(address.length() - 2, ' ');
+                }
             } else {
                 address.insert(0, ":x: : ");
+
+                // Ensure it's all the same length
+                while (address.length() < 32) {
+                    address.insert(address.length() - 2, ' ');
+                }
             }
 
-            if (version.length() > 18) {
-                version.setLength(18);
-                version.replace(version.length() - 2, version.length(), "``");
+            // Make sure version field is the same length always
+            if (version.length() > 10) {
+                version.setLength(8);
+                version.insert(version.length(), "``");
             }
 
-            while (version.length() < 18) {
-                version.insert(version.length() - 2, " ");
+            while (version.length() < 10) {
+                version.insert(version.length() - 2, ' ');
             }
 
-            while (address.length() < longestAddress + 18) {
-                address.insert(address.length() - 2, " ");
-            }
-
-            MessageEmbed.Field field = new MessageEmbed.Field(index + ". " + address + " - " + version + " - " + timestamp, "_ _", false);
+            MessageEmbed.Field field = new MessageEmbed.Field(String.format("%d. %s - %s - %s",
+                    index,
+                    address,
+                    version,
+                    String.format("<t:%d:R>", entry.get("last_seen").getAsLong())
+            ), "_ _", false);
             embed.addField(field);
             index++;
         }
