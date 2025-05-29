10e822378aebede364c7717886eba32b9c1b9733
funtimes909 <funtimes90969@proton.me>
2025-05-30 11:29:47 +1200

    fix(PlayerhistoryEmbedBuilder): Ensure playerhistory field lengths are consistent

--- bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/PlayerhistorySearchBuilder.java
+++ bot/src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/PlayerhistorySearchBuilder.java
@@ -19,11 +19,24 @@ public class PlayerhistorySearchBuilder {
             if (i > 9) break;
             JsonObject object = array.get(i).getAsJsonObject();
 
+            StringBuilder address = new StringBuilder(object.get("address").getAsString() + ":" + object.get("port").getAsShort());
+
+            // Index 10 has an extra indent
+            if ((i + 1) == 10) {
+                while (address.length() < 23) {
+                    address.insert(address.length(), " ");
+                }
+            } else {
+                // Ensure it's all the same length
+                while (address.length() < 24) {
+                    address.insert(address.length(), " ");
+                }
+            }
+
             // Add a field for every player
-            String field = String.format("%d. ``%s:%d`` ``%s`` <t:%d:R>",
+            String field = String.format("%d. ``%s`` ``%s`` <t:%d:R>",
                     (i + 1),
-                    object.get("address").getAsString(),
-                    object.get("port").getAsInt(),
+                    address,
                     object.get("name").getAsString(),
                     object.get("last_seen").getAsInt()
             );
