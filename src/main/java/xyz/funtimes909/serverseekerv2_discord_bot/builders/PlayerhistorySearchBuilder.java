338789a929570285c59acded92e57b14dbea2ccf
funtimes909 <funtimes90969@proton.me>
2025-06-01 17:14:51 +1200

    fix(playerhistory): Ensure player names are consistent lengths

--- src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/PlayerhistorySearchBuilder.java
+++ src/main/java/xyz/funtimes909/serverseekerv2_discord_bot/builders/PlayerhistorySearchBuilder.java
@@ -1,6 +1,7 @@
 package xyz.funtimes909.serverseekerv2_discord_bot.builders;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
@@ -15,14 +16,26 @@ public class PlayerhistorySearchBuilder {
                 .setTitle("Showing player history for: " + title)
                 .setColor(new Color(0, 255, 0));
 
+        int longestName = 0;
+
+        // The longest length name must be known before we create the embed
+        // First we have to iterate over all the elements to get the length
+        for (JsonElement element : array) {
+            JsonObject object = element.getAsJsonObject();
+
+            String name = object.get("name").getAsString();
+            if (name.length() > longestName) longestName = name.length();
+        }
+
         for (int i = 0; i < array.size(); i++) {
             if (i > 9) break;
             JsonObject object = array.get(i).getAsJsonObject();
 
             StringBuilder address = new StringBuilder(object.get("address").getAsString() + ":" + object.get("port").getAsShort());
+            StringBuilder name = new StringBuilder(object.get("name").getAsString());
 
             // Index 10 has an extra indent
-            if ((i + 1) == 10) {
+            if (i == 9) {
                 while (address.length() < 23) {
                     address.insert(address.length(), " ");
                 }
@@ -33,11 +46,16 @@ public class PlayerhistorySearchBuilder {
                 }
             }
 
+            // Ensure all player names are consistent lengths
+            while (name.length() < longestName) {
+                name.insert(name.length(), " ");
+            }
+
             // Add a field for every player
             String field = String.format("%d. ``%s`` ``%s`` <t:%d:R>",
                     (i + 1),
                     address,
-                    object.get("name").getAsString(),
+                    name,
                     object.get("last_seen").getAsInt()
             );
             embed.addField(field, "_ _", false);
